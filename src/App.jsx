f6066a6d290d441be95d0d886764a6f34b7a4f43
Jesus Molina <netod90@gmail.com>
2023-08-07 18:23:36 -0700

    Add state in turns

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
-import './App.css'
+import { useState } from 'react';
+
+const TURNS = {
+  X: 'X',
+  O: 'o',
+};
+
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
 
 function App() {
- return <h1>Tic tac toe</h1>
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {board[index]}
+            </Square>
+          );
+        })}
+      </section>
+
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
